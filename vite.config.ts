3dd7b6d14b24dac88c871354060043be951f6c40
Fredrik Zander Kloster <fredrik.kloster@vipps.no>
2024-09-09 09:05:51 +0200

    :card_file_box: Base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: 'gutta-open/',
   plugins: [
     remix({
+      basename: '/gutta-open/',
       ssr: false,
       future: {
         v3_fetcherPersist: true,
